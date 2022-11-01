514680db4b0653a633826f603c4c7d14f8004bcf
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-11-01 18:35:49 +0100

    .

--- ej3.py
+++ ej3.py
@@ -7,5 +7,23 @@ class Nave():
     def __str__ (self):
 #la f es lo mismpo que .format pero hay que pasarle las variables con el self
         print(f 'Nombre: {self.nombre} - Largo: {self.largo}')
-        print(f 'tripulacion: {self.tripulacion} - cantidad pasajeros: {self.cantidad_pasajeros}')
+        print(f 'tripulacion: {self.tripulacion} - cantidad pasajeros: {self.cantidad_pasajeros}') 
         
+def ordenar(lista_naves)
+#ordenar por letras
+
+
+
+
+
+
+
+
+
+#crear naves
+
+nave1 = ('Halcon milenario', 800,3,6)
+nave2 = ( 'Estrella de la muerte ')
+nave3 =
+nave4 =
+nave5 =
